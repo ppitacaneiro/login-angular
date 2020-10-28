63b8ecb2b57393e5facae5a2dbb16cc596aae94b
pablo <ppitacaneiro@gmail.com>
2020-10-28 19:26:47 +0100

    Creación del formulario de registro y validaciones correspondientes

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario:UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+  }
+
+  onSubmit(form:NgForm) {
+
+    if (form.invalid) { return; }
+    
+    console.log(this.usuario);
+    console.log(form);
+  }
+
+
+}
